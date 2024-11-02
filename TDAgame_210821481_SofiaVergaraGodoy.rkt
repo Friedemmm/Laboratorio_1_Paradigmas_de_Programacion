b40b98908cb47a07dfef5fba0649f81c0a4dda8a
Sofía Vergara Godoy <148838071+Friedemmm@users.noreply.github.com>
2024-11-01 23:01:52 -0300

    Final TDA
    Se unieron y modificaron funciones.

--- TDAgame_210821481_SofiaVergaraGodoy.rkt
+++ TDAgame_210821481_SofiaVergaraGodoy.rkt
@@ -1,71 +1,223 @@
-; Descripción: Crear una partida.
-; Dom: player1 (player) X player2 (player) X board (board) X current-turn (int).
-; Rec: Game.
-; Tipo recursión: No aplica.
+#lang racket
+
+(provide (all-defined-out))
 
 (require "TDAplayer_2108221481_SofiaVergaraGodoy.rkt")
 (require "TDApiece_2108221481_SofiaVergaraGodoy.rkt")
 (require "TDAboard_2108221481_SofiaVergaraGodoy.rkt")
 
+;----------------CONSTRUCTOR-GAME----------------;
+
+; Descripción: Crear una partida.
+; Dom: player1 (player) X player2 (player) X board (board) X current-turn (int).
+; Rec: Game.
+; Tipo recursión: No aplica.
+
 (define (game player1 player2 board turno)
   (crear-lista player1 player2 board turno)
   )
 
--------Funciones Auxiliares-----
+;----------------SELECTORES----------------;
+
+; Descripción: Obtiene el jugador cuyo turno esta en curso.
+; Dom: Game (game).
+; Rec: Player.
+; Tipo recursión: No aplica.
+
+(define (game-get-current-player game) ; Lista Game: player1 player2 board turno. ; Lista Player: id name color wins losses draws remaining-pieces.
+  (if (= (modulo (fourth-element game) 2) 1)
+      (first-element game) ; Si es impar, le toca a player1.
+      (second-element game) ; Si es par, le toca a player2.
+      )
+  )
+
+;-----------------------------------------;
+
+; Descripción: Entregar el estado actual del tablero.
+; Dom: Game (game).
+; Rec: Board.
+; Tipo recursión: No aplica.
+
+; Función para printear el tablero.
+(define (game-get-board game) ; Lista Game: player1 player2 board turno. 
+  (newline)
+  (third-element game)
+  )
+
+;---------------MODIFICADORES-----------------;
+
+; Descripción: Se finaliza el juego y actualiza las estadisticas de los jugadores según el resultado.|
+; Dom: Game (game).
+; Rec: Game.
+; Tipo recursión: No aplica.
+
+; Función que dependiendo del resultado llama a actualizar las stats.
+(define (game-set-end game)
+  (cond
+    ; Empate
+    [(equal? (game-is-draw? game) #t)
+     (list
+       (player-update-stats (first-element game) "draw")
+       (player-update-stats (second-element game) "draw")
+       (third-element game)
+       (fourth-element game))]
+    
+    ; Player1 gana
+    [(= (board-who-is-winner (third-element game)) 1)
+     (list
+       (player-update-stats (first-element game) "win")
+       (player-update-stats (second-element game) "loss")
+       (third-element game)
+       (fourth-element game))]
+    
+    ; Player2 gana
+    [(= (board-who-is-winner (third-element game)) 2)
+     (list
+       (player-update-stats (first-element game) "loss")
+       (player-update-stats (second-element game) "win")
+       (third-element game)
+       (fourth-element game))]
+    
+    ; No ha terminado
+    [else game]
+    )
+  )
+
+;-----------------------------------------;
+
+; Descripción: Verifica que sea turno del jugador, actualia el estado del juego después de un movimiento, verifica el ganador o empate y si terminó, actualiza las estadísticas.
+; Dom: game (game) X player (player) X column (int).
+; Rec: Game.
+; Tipo recursión: No aplica.
+
+; Función principal para mover y hacer todo lo demás.
+(define (game-player-set-move game player columna)
+  (cond
+    [(game-ended? game) (error "El juego ya ha terminado")] ; Verifica si el juego ya terminó antes de jugar.
+    
+    [(not (= (first-element player)(first-element (game-get-current-player game))))(error "No es tu turno")]
+    
+    [(= (seventh-element (if (= (first-element player) 1)(first-element game)(second-element game))) 0)(error "No tienes fichas disponibles")] ; Revisamos las fichas del player.
+    
+    [else (empate-o-otro
+           (crear-lista
+            (actualizar-jugador 1 player game
+                                (crear-lista (first-element player)
+                                             (second-element player)
+                                             (third-element player)
+                                             (fourth-element player)
+                                             (fifth-element player)
+                                             (sixth-element player)
+                                             (- (seventh-element (if (= (first-element player) 1)
+                                                                     (first-element game)
+                                                                     (second-element game)
+                                                                     )) 1)
+                                             )
+                                )
+            (actualizar-jugador 2 player game 
+                                (crear-lista (first-element player)
+                                             (second-element player)
+                                             (third-element player)
+                                             (fourth-element player)
+                                             (fifth-element player)
+                                             (sixth-element player)
+                                             (- (seventh-element (if (= (first-element player) 2)
+                                                                     (second-element game)
+                                                                     (first-element game)
+                                                                     )) 1)
+                                             )
+                                )
+            (board-set-play-piece 
+             (third-element game)
+             columna
+             (piece (third-element player))
+             )
+            (+ (fourth-element game) 1)
+            )
+           )
+          ]
+    )
+  )
+
+; Función auxiliar para actualizar al plater.
+(define (actualizar-jugador id current-player game updated-player)
+  (if (= (first-element current-player) id)
+      updated-player
+      (if (= id 1)
+          (first-element game) ; Usamos el player actual.
+          (second-element game)
+          )
+      )
+  ) 
+
+; Función auxliar para verificar que termino el juego.
+(define (game-ended? game)
+  (or (not (= (board-who-is-winner (third-element game)) 0))
+      (game-is-draw? game)))
+
+; Función auxliar para verificar que empataron o gano alguien.
+(define (empate-o-otro new-game)
+  (cond
+    [(not (= (board-who-is-winner (third-element new-game)) 0))(game-set-end new-game)]
+    [(game-is-draw? new-game)(game-set-end new-game)]
+    [else new-game]
+    )
+  )
+
+;-----------------OTROS---------------------;
 
 ; Descripción: Generar un historial de movimientos de la partida.
 ; Dom: Game (game).
 ; Rec: Lista de movimientos. Cada movimiento es un par (columna . color).
 ; Tipo recursión: No aplica.
 
-; Función para encontrar una pieza en el tablero
-(define (find-piece board fila columna)
+; Función auxiliar para obtener el color de una posición del board.
+(define (color-posicion board fila columna)
   (if (list? (cierto-element (cierto-element board fila) columna))
       (first-element (cierto-element (cierto-element board fila) columna))
-      0)
-)
-
-; Función para encontrar la fila donde cayó la pieza.
-(define (find-fila board columna)
-  (define (aux-find-fila fila)
-    (cond 
-      [(= fila 6) -1]  ; No se encontró fila (error).
-      [(list? (cierto-element (cierto-element board fila) columna)) fila]
-      [else (aux-find-fila (+ fila 1))]
+      "empty"
       )
+  )
+
+; Función auxiliar los mov en el board.
+(define (find-mov board initial-board fila columna acc)
+  (cond
+    [(>= fila 6) acc] ; Terminamos de recorrer.
+    [(>= col 7) (find-mov board initial-board (+ fila 1) 0 acc)] ; Siguiente fila.
+    [(not (equal? (color-posicion board fila columna)(color-posicion initial-board fila columna))) 
+     (find-mov board initial-board fila (+ columna 1)(my-cons (crear-lista fila columna (color-posicion board fila columna)) acc))
+     ]
+    [else (find-mov board initial-board fila (+ columna 1) acc)] ; Siguiente columna.
     )
-  (aux-find-fila 0)
+  )    
+
+; Función para asignar turnos a los movimientos encontrados.
+(define (asignar-turnos mov turno)
+  (if (null? mov)
+      '()
+      (my-cons (crear-lista turno
+                            (second-element (my-car mov))
+                            (third-element (my-car mov))
+                            )
+               (asignar-turnos (my-cdr mov) (- turno 1))
+               )
+      )
   )
 
-; Función auxiliar para crear una entrada en el historial
-(define (create-history-entry board turno columna fila)
-  (list turno columna (find-piece board fila columna)
+; Función para construir el historial.
+(define (game-history game)
+  (if (equal? (third-element game) (board))
+      '()
+      (reverse
+       (asignar-turnos 
+        (find-mov (third-element game) (board) 0 0 '())
+        (- (fourth-element game) 1)
         )
-  )
-
-; Función auxiliar para procesar una jugada para el historial
-(define (process-history-move board turno columna)
-  (define fila (find-fila board columna))
-  (if (= fila -1)
-      '() ; Error: no se encontró la última jugada
-      (cons (create-history-entry board turno columna fila)
-            (game-history (board) (- turno 1))
-            )
+       )
       )
   )
 
-; Función principal para construir el historial de jugadas.
-(define (game-history board turno)
-  (if (equal? board (board))  ; Si es tablero vacío.
-      '()  ; Retorna lista vacía.
-      (process-history-move board turno (- (fourth-element game) 1))
-      )
-  )
-
-; Iniciar construcción del historial con el tablero actual.
-(define (get-game-history game)
-  (game-history (third-element game) (fourth-element game))
-  )
+;----------------------------------------;
 
 ; Descripción: Verificar si los jugadores se quedaron sin fichas o no gano nadie y no queda espacio en el tablero.
 ; Dom: Game (game).
@@ -75,22 +227,24 @@
 ; Función para verificar empate.
 (define (game-is-draw? game) ; Lista Game: player1 player2 board turno. ; Lista Player: id name color wins losses draws remaining-pieces.
   (cond 
-    [(= (board-can-play? (third-element game)) #f) #t] ; Tablero lleno
+    [(equal? (board-can-play? (third-element game)) #f) #t] ; Tablero lleno
     [(and (= (seventh-element (first-element game)) 0)(= (seventh-element (second-element game)) 0)) #t] ; Si ambos tienen 0 fichas.          
     [else #f] ; No es empate.
     )
-  ) 
+  )
+
+;-----------------------------------------;
 
 ; Descripción: Actulizar las estadisticas del jugador, ya sea victoria, derrota o empate.
 ; Dom: player (player) X result (string: "win", "loss", o "draw").
-; Rec: player.
+; Rec: player
 ; Tipo recursión: No aplica.
 
-; Función para agregar +1 a la estadistica que termine el juego.
+; Función para agregar +1 dependiendo de el resultado.
 (define (player-update-stats player result)
   (cond ; Lista Player: id name color wins losses draws remaining-pieces.
     [(equal? result "win") 
-     (player (first-element player) ; ID
+     (crear-lista (first-element player) ; ID
              (second-element player) ; Nombre.
              (third-element player) ; Color.
              (+ (fourth-element player) 1) ; Victorias + 1.
@@ -101,19 +255,19 @@
      ]
     
     [(equal? result "loss") 
-     (player (first-element player) ; ID.
-             (second-element player) ; Nombre.
-             (third-element player) ; Color.
-             (fourth-element player) ; Victorias.
-             (+ (fifth-element player) 1) ; Derrotas + 1.
-             (sixth-element player) ; Empates.
-             (seventh-element player) ; Fichas.
-             )
+     (crear-lista (first-element player) ; ID.
+                 (second-element player) ; Nombre.
+                 (third-element player) ; Color.
+                 (fourth-element player) ; Victorias.
+                 (+ (fifth-element player) 1) ; Derrotas + 1.
+                 (sixth-element player) ; Empates.
+                 (seventh-element player) ; Fichas.
+                 )
      ]
   
     
     [(equal? result "draw")
-     (player (first-element player) ; ID.
+     (crear-lista (first-element player) ; ID.
              (second-element player) ; Nombre.
              (third-element player) ; Color.
              (fourth-element player) ; Victorias.
@@ -126,103 +280,13 @@
     [else player]
     )
   )       
+;-----------------------------------------;
 
-; Descripción: Obtiene el jugador cuyo turno esta en curso.
-; Dom: Game (game).
-; Rec: Player.
-; Tipo recursión: No aplica.
 
-(define (game-get-current-player game) ; Lista Game: player1 player2 board turno. ; Lista Player: id name color wins losses draws remaining-pieces.
-  (cond
-    [(> (seventh-element (first-element game))(seventh-element (second-element game))) first-element] ; Si player1 > player2 en fichas, le toca al player1.
-    [(< (seventh-element (first-element game))(seventh-element (second-element game))) second-element] ; Si player1 < player2 en fichas, le toca al player2.
-    [(= (seventh-element (first-element game))(seventh-element (second-element game))) first-element] ; Si tienen igual fichas, empieza player1.
-    [else game]
-    )
-  )
 
-; Descripción: Entregar el estado actual del tablero.
-; Dom: Game (game).
-; Rec: Board.
-; Tipo recursión: No aplica.
 
-; Función para printear el tablero.
-(define (game-get-board game) ; Lista Game: player1 player2 board turno. 
-  (print "Estado actual del tablero:")
-  (print (third-element game))
-  (third-element game)
-  )
 
-; Descripción: Se finaliza el juego y actualiza las estadisticas de los jugadores según el resultado.
-; Dom: Game (game).
-; Rec: Game.
-; Tipo recursión: No aplica.
 
-; Función que dependiendo del resultado llama a actualizar las stats.
-(define (game-set-end game)
-  (cond
-    ; Empate.
-    [(game-is-draw? game)
-     (player-update-stats (first-element game) "draw")
-     (player-update-stats (second-element game) "draw")
-     ]
 
-    ; Player1 gana.
-    [(= (board-who-is-winner (third-element game)) 1)
-     (player-update-stats (first-element game) "win")
-     (player-update-stats (second-element game) "loss")
-     ]
 
-    ; Player2 gana.
-    [(= (board-who-is-winner (third-element game)) 2)
-     (player-update-stats (first-element game) "loss")
-     (player-update-stats (second-element game) "win")
-     ]
-    )
-  )
 
-; Descripción: Verifica que sea turno del jugador, actualia el estado del juego después de un movimiento, veritidca el ganador o empate y si terminó, actualiza las estadísticas.
-; Dom: game (game) X player (player) X column (int).
-; Rec: Game.
-; Tipo recursión: No aplica.
-
-; Función auxiliar para restar fichas.
-(define (fichas-menos player)
-  (player (first-element player) ; ID
-          (second-element player) ; Nombre.
-          (third-element player) ; Color.
-          (fourth-element player) ; Victorias.
-          (fifth-element player) ; Derrotas.
-          (sixth-element player) ; Empates.
-          (-1 (seventh-element player)) ; Fichas - 1.
-          )
-  )
-; Función auxiliar aumentar turnos.
-(define (actualizar-turno game) ; Lista Game: player1 player2 board turno.
-  (game (first-element game)
-        (second-element game)
-        (third-element game)
-        (+1 (fourth-element game))
-        )
-  )
-  
-
-; Función principal que realiza el movimiento
-(define (game-player-set-move game player columna) ;  Lista Game: player1 player2 board turno. 
-  (if (game-set-end game)
-      (error "el juego ya ha terminado, no puede jugar")
-      (if (not (equal? (game-get-current-player game) player))
-          (error "le toca al otro jugador")
-          (if (board-set-play-piece (third-element game) player columna)
-              (if (fichas-menos player)
-                  (if (game-history game)
-                      (game-set-end game)
-                      "error al actualizar historial"
-                      )
-                  "error al actualizar fichas"
-                  )
-              "error al colocar pieza"
-              )
-          )
-      )
-  )
