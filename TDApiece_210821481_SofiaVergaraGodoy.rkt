4a58743c20569fa33b68b78ee465834998fb3811
Sofía Vergara Godoy <148838071+Friedemmm@users.noreply.github.com>
2024-11-01 23:02:19 -0300

    Final TDA
    Se ordeno.

--- TDApiece_210821481_SofiaVergaraGodoy.rkt
+++ TDApiece_210821481_SofiaVergaraGodoy.rkt
@@ -1,9 +1,15 @@
+#lang racket
+
+(provide (all-defined-out))
+
+(require "TDAplayer_2108221481_SofiaVergaraGodoy.rkt")
+
+;----------------CONSTRUCTOR-PIECE----------------;
+
 ; Descripción: Crea una ficha, validando que sea string para luego transformarlo en lista.
 ; Dom: Color (string).
 ; Rec: La pieza creada (piece).
 ; Tipo recursión: No aplica.
-
-(require "TDAplayer_2108221481_SofiaVergaraGodoy.rkt")
 
 ; Función para crear una ficha.
 (define (piece color)
@@ -11,4 +17,6 @@
       (error "color debe ser una cadena de caracteres")
       (crear-lista color)
       )
-  ) 
+  )
+
+;-----------------------------------------;
